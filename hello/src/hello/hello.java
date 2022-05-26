78208c0f1d04dd84043a0aaa666d7ff97224305e
wltjs2851 <56398560+wltjs2851@users.noreply.github.com>
2022-05-26 13:49:22 +0900

    Update hello.java

--- hello/src/hello/hello.java
+++ hello/src/hello/hello.java
@@ -5,7 +5,7 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
-		System.out.println("hello2");
+		System.out.println("hello3");
 	}
 
 }
